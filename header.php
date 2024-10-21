7cbf3cd119e7d639554f2a67c4c84512d17973e9
Abhishek Singh <167921253+Abhishek-Singh8924@users.noreply.github.com>
2024-10-21 16:35:46 +0530

    first commit

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learn wave</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="https://img.freepik.com/free-vector/aqua-business-logo-template-professional-creative-color-flat-design-vector_53876-146807.jpg" type="image/x-icon">
+    <style>
+        .header-img{
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: auto;
+                width: 100%;
+                background-color: #a9a9a926;
+        }
+    </style>
+</head>
+<body>
+<header>
+    <div class="header-img">
+        <img src="https://i.pinimg.com/originals/47/5b/59/475b59c61a6efb3ec8ac0a57e43fd03b.webp" alt="" width="20%">
+    </div>
+</header>
